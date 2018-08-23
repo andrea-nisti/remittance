841cb4ff568c08d3dd060777e8f2c84b8a145ef9
Andrea Nisticò <andrea.nistic@gmail.com>
2018-08-23 17:59:20 +0200

    fix giveMeMoney and add stop switch

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -13,25 +13,27 @@ contract Remittance is Owned {
 	struct DepositStruct {
 		uint    value;
 	 	uint256 time;
+	 	address exchangeAddr;
 	}
 	
 	mapping (bytes32 => DepositStruct) public deposits;
 	
 	constructor () public  {}
 	
-	function deposit(bytes32 puzzle) public payable  only_owner{
+	function deposit(bytes32 puzzle, ) public payable  only_owner{
 
 		//Requires
 		require (msg.value > 0);
 		require (deposits[puzzle].value == 0, "Puzzle already set");
 		//Create new deposit
 		deposits[puzzle] = DepositStruct(msg.value,now);
-	}	
+	}
 	
 	function giveMeMoney (string pass1, string pass2) public returns(bool res){
-		
+				
 		bytes32 hashish = UtilsLib.getKekka(pass1,pass2);
 		require (deposits[hashish].value > 0);
+		require (msg.sender == deposits[hashish].exchangeAddr);
 
 		//Transfer amount
 		deposits[hashish].value = 0;
@@ -39,5 +41,11 @@ contract Remittance is Owned {
 		
 		return true;		
 	}
+
+	//Stop switch
+	function killMe() public {
+    	assert(msg.sender == owner);
+    	selfdestruct(owner);
+    }
 
 }
