ccc58cec279f9b323965b93aa7db683ec26e2315
Andrea Nisticò <andrea.nistic@gmail.com>
2018-08-23 19:55:13 +0200

    eventing and better logic

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -13,6 +13,10 @@ contract Remittance is Owned {
 	mapping (bytes32 => DepositStruct) public deposits;
 	
 	constructor () public  {}
+
+	//Events
+	event LogNewDeposit(bytes32 puzzle, uint value);
+	event LogNewWithdraw(address exchangeAddr, uint amount);
 	
 	function deposit(bytes32 puzzle, address exchangeAddr) public payable  only_owner{
 
@@ -20,18 +24,21 @@ contract Remittance is Owned {
 		require (msg.value > 0);
 		require (deposits[puzzle].value == 0, "Puzzle already set");
 		//Create new deposit
+		emit LogNewDeposit(puzzle, msg.value);
 		deposits[puzzle] = DepositStruct(msg.value,now,exchangeAddr);
 	}
-
 	function giveMeMoney (string pass1, string pass2) public returns(bool res){
 		
-		bytes32 hashish = UtilsLib.getKekka(pass1,pass2);
-		require (deposits[hashish].value > 0);
-		require (msg.sender == deposits[hashish].exchangeAddr);
+		uint tAmount      = deposits[hashish].value;
+		address tExchange = deposits[hashish].exchangeAddr;
+		bytes32 hashish   = UtilsLib.getKekka(pass1,pass2);
+		
+		require (tAmount > 0);
+		require (msg.sender == tExchange);
 
 		//Transfer amount
-		uint tAmount = deposits[hashish].value;
 		deposits[hashish].value = 0;
+		emit LogNewWithdraw(tExchange, tAmount);
 		msg.sender.transfer(tAmount);
 		
 		return true;		
