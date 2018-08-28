7c01a013b50bc18339a2c8f8f50dfee6255d162d
Andrea Nisticò <andrea.nistic@gmail.com>
2018-08-28 16:59:22 +0200

    convert tab to spaces

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.4.23;
 
 contract Owned {
-	address public owner;
-	constructor () public{
-		owner = msg.sender;
-	}
-	event OwnerChanged(address indexed old, address indexed current);
-	
-	modifier onlyOwner { 
-		require(msg.sender == owner);
-		 _;
-	}
-	function setOwner(address _newOwner) onlyOwner public {
-		emit OwnerChanged(owner, _newOwner); 
-		owner = _newOwner; 
-	}
+    address public owner;
+    constructor () public{
+        owner = msg.sender;
+    }
+    event OwnerChanged(address indexed old, address indexed current);
+    
+    modifier onlyOwner { 
+        require(msg.sender == owner);
+        _;
+    }
+    function setOwner(address _newOwner) onlyOwner public {
+        emit OwnerChanged(owner, _newOwner); 
+        owner = _newOwner; 
+    }
 
 }
